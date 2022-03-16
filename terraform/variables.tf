9635f31519fb05a4739947d8a9ac5701e0d253e8
Airat <60946008+Meeki1@users.noreply.github.com>
2022-03-16 03:10:13 +0300

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -6,13 +6,13 @@ variable "aws_region" {
 variable "aws_access_key" {
   type = string
   sensitive   = true
-  description = "AKIAXFRV6JVGZTQ4ZYGF"
+  description = "Your access key"
 }
 
 variable "aws_secret_key" {
   type = string
   sensitive   = true
-  description = "Cf6uMVMIORlfvlbgCT3P6BpqFw33kK82/tvLrerA"
+  description = "Your secret key"
 }
 
 
